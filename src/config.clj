6d148850fd0d99b98793d59bf9cf6a98d7cf0537
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-06-25 21:12:36 +0200

    convert session secret to byte array instead of string as required by the ring middleware

--- src/config.clj
+++ src/config.clj
@@ -3,6 +3,17 @@
             [aero.core :as aero]
             [clojure.java.io :as io]))
 
+(defn- str->byte-arr
+  "Converts a string to a Java byte array."
+  [s]
+  (into-array Byte/TYPE (map byte s)))
+
+(defn- config
+  []
+  (-> (io/resource "config.edn")
+      (aero/read-config)
+      (update :session-secret str->byte-arr)))
+
 (declare cfg)
 (defstate cfg
-  :start (aero/read-config (io/resource "config.edn")))
+  :start (config))
