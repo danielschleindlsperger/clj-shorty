40fe211d60cda0d9740b118fe3e7bd3adb61aa74
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-08-03 12:34:57 +0200

    add ssh tunneling capabilities to sqitch migration action

--- .github/actions/sqitch-migrate/entrypoint.sh
+++ .github/actions/sqitch-migrate/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -l
 
-set -e
+set -eu
+
+if [ -n "${INPUT_SSH_TUNNEL_ENABLE}" ]; then
+  echo "Enabling SSH tunnel mapping."
+  sshpass -p "${INPUT_SSH_TUNNEL_PASSWORD}" sh -c 'ssh -fNT -o StrictHostKeyChecking=no -L "${INPUT_SSH_TUNNEL_MAPPING}" "${INPUT_SSH_TUNNEL_DESTINATION}" -p "${INPUT_SSH_TUNNEL_PORT}" && sleep 2' 
+fi
 
 sqitch deploy "${INPUT_DATABASE_URL}"
