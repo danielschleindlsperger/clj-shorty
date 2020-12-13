36346f3591b655232b80e789513cf25b8b5175b4
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-12-13 16:45:29 +0100

    log hostname on server startup

--- src/clj_shorty/server.clj
+++ src/clj_shorty/server.clj
@@ -1,9 +1,14 @@
 (ns clj-shorty.server
   (:gen-class)
   (:require [integrant.core :as ig]
+            [taoensso.timbre :as log]
             [org.httpkit.server :refer [run-server]]))
 
 (defmethod ig/init-key :clj-shorty/server [_ {:keys [config routes]}]
-  (run-server routes {:port (:port config)}))
+  (let [port (:port config)
+        server (run-server routes {:port port})]
+    (log/info (format "Server running @ http://localhost:%s" port))
+    server))
 
-(defmethod ig/halt-key! :clj-shorty/server [_ server] (server))
+(defmethod ig/halt-key! :clj-shorty/server [_ server]
+  (when server (server)))
