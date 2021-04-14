fad3360e241008390ec61209c2221bd1f8f74622
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2021-04-14 22:45:10 +0200

    add REPL hints on startup

--- dev/user.clj
+++ dev/user.clj
@@ -2,6 +2,10 @@
 
 (set! *warn-on-reflection* true)
 
+(println "Loaded namespace `user`, welcome to clj-shorty!")
+(println "Run (dev) and (go) to start the application.")
+(println "Run (integrant.repl/reset) to reset after code changes")
+
 (defn dev []
   (require 'dev)
   (in-ns 'dev))
