c993a7f76ba89a7011ec4b18faad0f67480056cf
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2021-08-28 15:49:30 +0200

    make sure app can connect to postgres when db url is missing 'jdbc:' prefix

--- src/clj_shorty/database.clj
+++ src/clj_shorty/database.clj
@@ -1,11 +1,17 @@
 (ns clj-shorty.database
   (:require [integrant.core :as ig]
-            [next.jdbc :as jdbc]
+            [clojure.string :as str]
+            [next.jdbc]
             [next.jdbc.connection :as connection])
   (:import (com.zaxxer.hikari HikariDataSource)))
 
+;; Make sure the db url is a jdbc compliant one, i.e. it starts with "jdbc:"
+;; Some cloud providers will use the "postgres://" format.
+(defn- jdbc-url [db-url]
+  (if (str/starts-with? db-url "jdbc:") db-url (str "jdbc:" db-url)))
+
 (defmethod ig/init-key :clj-shorty/database [_ {:keys [config]}]
-  (let [db-spec {:jdbcUrl (:db-conn-string config)}]
+  (let [db-spec {:jdbcUrl (jdbc-url (:db-conn-string config))}]
     (connection/->pool HikariDataSource db-spec)))
 
 (defmethod ig/halt-key! :clj-shorty/database [_ ds]
